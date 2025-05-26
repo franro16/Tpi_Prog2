9602ca83fd26aba8eca9486805a1e1737223617a
Aaagustin <agusxxheredia@gmail.com>
2025-05-26 20:19:26 -0300

    aca aguregue las 2 clases y algunos atributos, tengo el main echo para estas dos clases, boxeador y categoria con el peso y record boxeador

---
+++ src/models/Boxeador.java
@@ -0,0 +1,65 @@
+package models;
+
+public class Boxeador {
+    private String nombre;
+    private String apellido;
+    private Categoria categoria;
+    private Genero genero;
+    private RecordBoxeador recorboxeador;
+
+    public Boxeador(String nombre, String apellido, Categoria categoria, Genero genero, RecordBoxeador recorboxeador) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.categoria = categoria;
+        this.genero = genero;
+        this.recorboxeador = recorboxeador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public RecordBoxeador getRecorboxeador() {
+        return recorboxeador;
+    }
+
+    public void setRecorboxeador(RecordBoxeador recorboxeador) {
+        this.recorboxeador = recorboxeador;
+    }
+
+    public void asignarCategoria(Categoria nuevaCategoria) {
+        this.categoria = nuevaCategoria;
+    }
+
+    public Boxeador getBoxeador() {
+        return this;
+    }
+}
---
+++ src/models/Categoria.java
@@ -0,0 +1,52 @@
+package models;
+
+public class Categoria {
+    private String nombre;
+    private double pesoMaximo;
+    private double pesoMinimo;
+    private String verCategoria;
+
+    public Categoria(String nombre, double pesoMaximo, double pesoMinimo, String verCategoria) {
+        this.nombre = nombre;
+        this.pesoMaximo = pesoMaximo;
+        this.pesoMinimo = pesoMinimo;
+        this.verCategoria= verCategoria;
+    }
+
+    public double getPesoMinimo() {
+        return pesoMinimo;
+    }
+
+    public void setPesoMinimo(double pesoMinimo) {
+        this.pesoMinimo = pesoMinimo;
+    }
+
+    public double getPesoMaximo() {
+        return pesoMaximo;
+    }
+
+    public void setPesoMaximo(double pesoMaximo) {
+        this.pesoMaximo = pesoMaximo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getVerCategoria() {
+        return verCategoria;
+    }
+
+    public void setVerCategoria(String verCategoria) {
+        this.verCategoria = verCategoria;
+    }
+
+    public String verCategoria() {
+        return "Categoría: " + nombre + " | Peso mínimo: " + pesoMinimo + "kg | Peso máximo: " + pesoMaximo + "kg";
+    }
+
+}
