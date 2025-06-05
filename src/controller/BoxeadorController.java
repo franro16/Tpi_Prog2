c9ba615098d5b717540c78bcbe6e48161d647eb8
valeledesma19 <valentinaledesma2407@gmail.com>
2025-06-05 17:57:49 -0300

    cargar archivos y guardar boxeadores en archivo

--- src/controller/BoxeadorController.java
+++ src/controller/BoxeadorController.java
@@ -5,6 +5,7 @@ import models.Categoria;
 import models.Genero;
 import view.ConsolaView;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +22,7 @@ public class BoxeadorController {
 //        categorias.add(new Categoria("Peso Mediano", 75.0, 60.1));
 //        categorias.add(new Categoria("Peso Pesado", 120.0, 75.1));
 //    }
+    private final String RUTA_ARCHIVO = "src/resources/boxeadores.txt";
     public BoxeadorController() {
         categorias = definirCategorias();
         campeonatosPorCategoriaGenero = new HashMap<>();
@@ -33,6 +35,49 @@ public class BoxeadorController {
         lista.add(new Categoria("Ligero", 60, 55.01));
         lista.add(new Categoria("Mediano", 72, 60.01));
         lista.add(new Categoria("Superpesado", Double.MAX_VALUE, 90.01));
+        return lista;
+    }
+    private void guardarBoxeadorEnArchivo(Boxeador b, String ruta) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(ruta, true))) { // modo append
+
+            String linea = b.getNombre() + "," + b.getApellido() + "," + b.getPeso() + "," +
+                    b.getCategoria().getNombre() + "," + b.getGenero().toString();
+            bw.write(linea);
+            bw.newLine();
+        } catch (IOException e) {
+            vista.mostrarMensaje("Error guardando boxeador en archivo: " + e.getMessage());
+        }
+    }
+    private List<Boxeador> cargarBoxeadoresDesdeArchivo(String ruta) {
+        List<Boxeador> lista = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] partes = linea.split(",");
+                if (partes.length != 5) continue;
+
+                String nombre = partes[0].trim();
+                String apellido = partes[1].trim();
+                double peso = Double.parseDouble(partes[2].trim());
+                String genStr = partes[4].trim().toUpperCase();
+
+                Genero genero;
+                try {
+                    genero = Genero.valueOf(genStr);
+                } catch (Exception e) {
+                    continue;
+                }
+
+                Boxeador b = new Boxeador(nombre, apellido, peso);
+                b.setGenero(genero);
+
+
+                asignarCategoria(b);
+                lista.add(b);
+            }
+        } catch (Exception e) {
+            vista.mostrarMensaje("Error leyendo archivo: " + e.getMessage());
+        }
         return lista;
     }
 //
