032b5109db7b0c713092265ba0b92efac4311c38
valeledesma19 <valentinaledesma2407@gmail.com>
2025-06-05 17:39:44 -0300

    Modifique FaseTorneo, le agregué un metodo

--- src/models/FaseTorneo.java
+++ src/models/FaseTorneo.java
@@ -4,5 +4,21 @@ public enum FaseTorneo {
     OCTAVOS,
     CUARTOS,
     SEMIFINAL,
-    FINAL
+    FINAL;
+    public FaseTorneo siguienteFase() {
+        switch (this) {
+            case OCTAVOS:
+                return CUARTOS;
+            case CUARTOS:
+                return SEMIFINAL;
+            case SEMIFINAL:
+                return FINAL;
+            case FINAL:
+                return FINAL; // no hay siguiente
+            default:
+                return null;
+        }
+    }
+
+
 }
