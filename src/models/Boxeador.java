55a39f28c3808fd6c48fde743f20bd286c1ed4b5
Aaagustin <agusxxheredia@gmail.com>
2025-06-14 17:03:12 -0300

    Modifiqué en un 50% la lista de uniciador, y el historial lo borré por completo, y lo hice más que nada más óptimo.

--- src/models/Boxeador.java
+++ src/models/Boxeador.java
@@ -1,122 +1,122 @@
-package models;
-
-import java.util.ArrayList;
-
-public class Boxeador {
-    private String nombre;
-    private String apellido;
-    private double peso;
-    private Categoria categoria;
-    private Genero genero;
-    private Historial historial;
-
-    private int victorias =0;
-    private int derrotas = 0;
-    private int empates = 0;
-    private int victoriasKO =0;
-
-    // Constructor
-    public Boxeador(String nombre, String apellido, double peso) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.peso = peso;
-        this.historial = new Historial(this);
-    }
-
-    public Historial getHistorial() {
-        return historial;
-    }
-
-    // Getters y Setters
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public double getPeso() {
-        return peso;
-    }
-
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-
-    public Genero getGenero() {
-        return genero;
-    }
-
-    public void setGenero(Genero genero) {
-        this.genero = genero;
-    }
-
-
-    //estos son metdos para que historial pueda modificar estadísticas
-    public void incrementarVictorias() {
-        victorias++;
-    }
-
-    public void incrementarDerrotas() {
-        derrotas++;
-    }
-
-    public void incrementarEmpates() {
-        empates++;
-    }
-
-    public void incrementarVictoriasKO() {
-        victoriasKO++;
-    }
-
-    public int getVictorias() {
-        return victorias;
-    }
-
-    public int getDerrotas() {
-        return derrotas;
-    }
-
-    public int getEmpates() {
-        return empates;
-    }
-
-    public int getVictoriasKO() {
-        return victoriasKO;
-    }
-
-
-    // toString para mostrar datos del boxeador
-    @Override
-    public String toString() {
-        String nombreCategoria = (categoria != null) ? categoria.getNombre() : "Sin categoría";
-        String generoStr = (genero != null) ? genero.toString() : "Sin género";
-
-        return "Boxeador: " + nombre + " " + apellido +
-                " | Peso: " + peso + " kg" +
-                " | Categoría: " + nombreCategoria +
-                " | Género: " + generoStr +
-                " | Victorias: " + victorias +
-                " | Derrotas: " + derrotas +
-                " | Empates: " + empates +
-                " | Victorias KO: " + victoriasKO;
-    }
-}
+//package models;
+//
+//import java.util.ArrayList;
+//
+//public class Boxeador {
+//    private String nombre;
+//    private String apellido;
+//    private double peso;
+//    private Categoria categoria;
+//    private Genero genero;
+//    private Historial historial;
+//
+//    private int victorias =0;
+//    private int derrotas = 0;
+//    private int empates = 0;
+//    private int victoriasKO =0;
+//
+//    // Constructor
+//    public Boxeador(String nombre, String apellido, double peso) {
+//        this.nombre = nombre;
+//        this.apellido = apellido;
+//        this.peso = peso;
+//        this.historial = new Historial(this);
+//    }
+//
+//    public Historial getHistorial() {
+//        return historial;
+//    }
+//
+//    // Getters y Setters
+//
+//    public String getNombre() {
+//        return nombre;
+//    }
+//
+//    public void setNombre(String nombre) {
+//        this.nombre = nombre;
+//    }
+//
+//    public String getApellido() {
+//        return apellido;
+//    }
+//
+//    public void setApellido(String apellido) {
+//        this.apellido = apellido;
+//    }
+//
+//    public double getPeso() {
+//        return peso;
+//    }
+//
+//    public void setPeso(double peso) {
+//        this.peso = peso;
+//    }
+//
+//    public Categoria getCategoria() {
+//        return categoria;
+//    }
+//
+//    public void setCategoria(Categoria categoria) {
+//        this.categoria = categoria;
+//    }
+//
+//    public Genero getGenero() {
+//        return genero;
+//    }
+//
+//    public void setGenero(Genero genero) {
+//        this.genero = genero;
+//    }
+//
+//
+//    //estos son metdos para que historial pueda modificar estadísticas
+//    public void incrementarVictorias() {
+//        victorias++;
+//    }
+//
+//    public void incrementarDerrotas() {
+//        derrotas++;
+//    }
+//
+//    public void incrementarEmpates() {
+//        empates++;
+//    }
+//
+//    public void incrementarVictoriasKO() {
+//        victoriasKO++;
+//    }
+//
+//    public int getVictorias() {
+//        return victorias;
+//    }
+//
+//    public int getDerrotas() {
+//        return derrotas;
+//    }
+//
+//    public int getEmpates() {
+//        return empates;
+//    }
+//
+//    public int getVictoriasKO() {
+//        return victoriasKO;
+//    }
+//
+//
+//    // toString para mostrar datos del boxeador
+//    @Override
+//    public String toString() {
+//        String nombreCategoria = (categoria != null) ? categoria.getNombre() : "Sin categoría";
+//        String generoStr = (genero != null) ? genero.toString() : "Sin género";
+//
+//        return "Boxeador: " + nombre + " " + apellido +
+//                " | Peso: " + peso + " kg" +
+//                " | Categoría: " + nombreCategoria +
+//                " | Género: " + generoStr +
+//                " | Victorias: " + victorias +
+//                " | Derrotas: " + derrotas +
+//                " | Empates: " + empates +
+//                " | Victorias KO: " + victoriasKO;
+//    }
+//}
--- src/models/Historial.java
+++ src/models/Historial.java
@@ -1,42 +1,42 @@
-package models;
-import models.*;
-import java.util.ArrayList;
-
-public class Historial {
-    private Boxeador boxeador;
-    private ArrayList<Combate> combates;
-
-    public Historial(Boxeador boxeador) {
-        this.boxeador = boxeador;
-        this.combates = new ArrayList<>();
-    }
-
-    public Boxeador getBoxeador() {
-        return boxeador;
-    }
-
-    public ArrayList<Combate> getCombates() {
-        return combates;
-    }
-
-    public void agregarCombate(Combate combate) {
-        combates.add(combate);
-    }
-
-    // Métodos para actualizar estadísticas según resultado
-    public void registrarVictoria(boolean porKO) {
-        boxeador.incrementarVictorias();
-        if (porKO) {
-            boxeador.incrementarVictoriasKO();
-        }
-    }
-
-    public void registrarDerrota() {
-        boxeador.incrementarDerrotas();
-    }
-
-    public void registrarEmpate() {
-        boxeador.incrementarEmpates();
-    }
-}
-
+//package models;
+//import models.*;
+//import java.util.ArrayList;
+//
+//public class Historial {
+//    private Boxeador boxeador;
+//    private ArrayList<Combate> combates;
+//
+//    public Historial(Boxeador boxeador) {
+//        this.boxeador = boxeador;
+//        this.combates = new ArrayList<>();
+//    }
+//
+//    public Boxeador getBoxeador() {
+//        return boxeador;
+//    }
+//
+//    public ArrayList<Combate> getCombates() {
+//        return combates;
+//    }
+//
+//    public void agregarCombate(Combate combate) {
+//        combates.add(combate);
+//    }
+//
+//    // Métodos para actualizar estadísticas según resultado
+//    public void registrarVictoria(boolean porKO) {
+//        boxeador.incrementarVictorias();
+//        if (porKO) {
+//            boxeador.incrementarVictoriasKO();
+//        }
+//    }
+//
+//    public void registrarDerrota() {
+//        boxeador.incrementarDerrotas();
+//    }
+//
+//    public void registrarEmpate() {
+//        boxeador.incrementarEmpates();
+//    }
+//}
+//
